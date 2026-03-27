03bdce20e134adfffe2a97ec5b3b339e3e7aa4f3
Gabriel <gabrieldaherm@gmail.com>
2026-03-27 19:07:03 -0300

    Refactor menu navigation: implement arrow key support and improve display logic

--- placeholders/pg_menu.py
+++ placeholders/pg_menu.py
@@ -6,47 +6,47 @@ index = 0
 key = 0
 
 # this code is so much cooler than i expected
+def seta():
+    while True:
+        os.system("cls")
+        # receive the info of pressed arrows on the keyboard
 
-while True:
-    os.system("cls")
-    # receive the info of pressed arrows on the keyboard
+        if key == b'\xe0':  # Arrow keys prefix
+            key = msvcrt.getch()
+            if key == b'K':  # Left arrow
+                index -= 1
+            elif key == b'M':  # Right arrow
+                index += 1
+            if key == b'q':  # Quit on 'q' key
+                break
 
-    if key == b'\xe0':  # Arrow keys prefix
+        match index:
+            case 0:
+                opt1 = ">>"
+                opt2 = "  "
+                opt3 = "  "
+                opt4 = "  "
+            case 1:
+                opt1 = "  "
+                opt2 = ">>"
+                opt3 = "  "
+                opt4 = "  "
+            case 2:
+                opt1 = "  "
+                opt2 = "  "
+                opt3 = ">>"
+                opt4 = "  "
+            case 3:
+                opt1 = "  "
+                opt2 = "  "
+                opt3 = "  "
+                opt4 = ">>"
+            case _:
+                index = 0
+                opt1 = ">>"
+                opt2 = "  "
+                opt3 = "  "
+                opt4 = "  "
+
+        print(f"{opt1} Start    {opt2} Load    {opt3} Options    {opt4} Exit")
         key = msvcrt.getch()
-        if key == b'K':  # Left arrow
-            index -= 1
-        elif key == b'M':  # Right arrow
-            index += 1
-        if key == b'q':  # Quit on 'q' key
-            break
-
-    match index:
-        case 0:
-            opt1 = ">>"
-            opt2 = "  "
-            opt3 = "  "
-            opt4 = "  "
-        case 1:
-            opt1 = "  "
-            opt2 = ">>"
-            opt3 = "  "
-            opt4 = "  "
-        case 2:
-            opt1 = "  "
-            opt2 = "  "
-            opt3 = ">>"
-            opt4 = "  "
-        case 3:
-            opt1 = "  "
-            opt2 = "  "
-            opt3 = "  "
-            opt4 = ">>"
-        case _:
-            index = 0
-            opt1 = ">>"
-            opt2 = "  "
-            opt3 = "  "
-            opt4 = "  "
-
-    print(f"{opt1} Start    {opt2} Load    {opt3} Options    {opt4} Exit")
-    key = msvcrt.getch()
--- shatteredsouls/sistemas/menu/menu.py
+++ shatteredsouls/sistemas/menu/menu.py
@@ -1,41 +1,78 @@
 from entidades import Entity
 from char_creation import cena
-
+import msvcrt
+import os
 
 print('Bem vindo ao jogo!')
 
+
+def seta():
+    index = 0
+
+    while True:
+        os.system('cls')
+
+        if index == 0:
+            opt1, opt2, opt3, opt4 = '>>', '  ', '  ', '  '
+        elif index == 1:
+            opt1, opt2, opt3, opt4 = '  ', '>>', '  ', '  '
+        elif index == 2:
+            opt1, opt2, opt3, opt4 = '  ', '  ', '>>', '  '
+        elif index == 3:
+            opt1, opt2, opt3, opt4 = '  ', '  ', '  ', '>>'
+        else:
+            index = 0
+            opt1, opt2, opt3, opt4 = '>>', '  ', '  ', '  '
+
+        print('Navegue com as setas e confirme com Enter ("q" para sair)')
+        print(f"{opt1} Start    {opt2} Load    {opt3} Options    {opt4} Exit")
+
+        key = msvcrt.getch()
+
+        if key == b'q':
+            return None
+
+        if key == b'\xe0':
+            key = msvcrt.getch()
+            if key == b'K':
+                index = (index - 1) % 4
+            elif key == b'M':
+                index = (index + 1) % 4
+            continue
+
+        if key == b'\r':
+            return index
+
+
 def menu():
+    print('=' * 25)
+    escolha = seta()
 
-    try:
-        print('=' * 25)
-        escolha = int(input('Escolha uma opção '))
-        
-        match escolha:
-            case 1:
-                print('=' * 25)
-                print('Você entrou no jogo! :)') 
-                return False
-            case 2:
-                print('=' * 25)
-                print('Você saiu do jogo. :(')
+    if escolha is None:
+        print('Saindo do menu...')
+        return False
 
-                return False
-            case 3:
-                print('=' * 25)
-                print('Carregando...')
-                return False
-            case 4:
-                print('=' * 25)
-                print('Opções:')
-                return False
+    match escolha:
+        case 0:
+            print('=' * 25)
+            print('Você entrou no jogo! :)')
+            return False
+        case 1:
+            print('=' * 25)
+            print('Carregando jogo salvo...')
+            return True
+        case 2:
+            print('=' * 25)
+            print('Opções: (ainda não implementado)')
+            return True
+        case 3:
+            print('=' * 25)
+            print('Você saiu do jogo. :(')
+            return False
 
-    except:
-        print('=' * 25)
-        print('Ocorreu um erro.')
-        print('=' * 25)
-        return True
+
 repetir = True
-#caso repetir for true, a função vai se repetir, caso for false, vai passar para a próx etapa.
+# caso repetir for true, a função vai se repetir, caso for false, vai passar para a próx etapa.
 
 while repetir:
     repetir = menu()
